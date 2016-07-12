427c6471c390a94be6698a3fc048e020002b92e1
Justin Holmes <justinmichaelholmes@gmail.com>
2016-07-12 11:14:18 -0600

    added some roles

--- bootStrapAnsible.sh
+++ bootStrapAnsible.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-sudo dnf install -y python-pip glibc-devel redhat-rpm-config openssl-devel python-devel python-dnf libselinux-python;
+sudo dnf install -y python-pip glibc-devel redhat-rpm-config openssl-devel python-devel python-dnf libselinux-python libsemanage-python;
 sudo pip install ansible==2.0.2.0
